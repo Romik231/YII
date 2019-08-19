c80719629edc726cd52b6df9f1d34ebf896c8ac1
Romik231 <Romik231@yandex.ru>
2019-08-19 15:33:38 +0300

    Закончил с мграциями

--- migrations/m190818_164153_Insert.php
+++ migrations/m190818_164153_Insert.php
@@ -13,8 +13,25 @@ class m190818_164153_Insert extends Migration
     public function safeUp()
     {
         $this->insert('users',[
-
+            'id'=>1,
+            'email'=>'test@email.ru',
+            'password_hash'=>'dsadadd',
         ]);
+
+        $this->insert('users',[
+            'id'=>2,
+            'email'=>'test2@email.ru',
+            'password_hash'=>'fdsfsff',
+        ]);
+
+        $this->batchInsert('activity',['title','startDate','user_id','useNotification' ],[
+            [Yii::$app->security->generateRandomString(),date('Y-m-d'),1,0],
+            [Yii::$app->security->generateRandomString(),date('Y-m-d'),2,0],
+            [Yii::$app->security->generateRandomString(),date('Y-m-d'),1,1],
+            [Yii::$app->security->generateRandomString(),date('Y-m-d'),2,1],
+            [Yii::$app->security->generateRandomString(),date('Y-m-d'),1,0],
+            [Yii::$app->security->generateRandomString(),'2019-08-19',1,0],
+            ]);
     }
 
     /**
@@ -22,9 +39,8 @@ class m190818_164153_Insert extends Migration
      */
     public function safeDown()
     {
-        echo "m190818_164153_Insert cannot be reverted.\n";
-
-        return false;
+        $this->delete('users');
+        $this->delete('activity');
     }
 
     /*
